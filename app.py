21c97ff7abbda90187d6b65125c5671daa27856e
Ahuitz05 <Ahuitzrojas@outlook.com>
2022-05-17 19:16:01 -0500

    data_collector_V1: created a data colector that doesn't go into all tabs (because of repeated columns in the last one) and creates a csv when done. note: could make it so that it collects all data in one citing and adds it directly to database, then if stoped it could look at last entry and continue from there

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
+from doctest import master
 from unicodedata import name
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-import os
+from selenium.webdriver.common.by import By
+
+import pandas as pd
 import time
 
 # Constants
 
 URL = "https://pnmi.segob.gob.mx/reporte/"
-TAB_NAMES = ['Información sobre el medio impreso', 'Información adicional sobre el medio impreso', 'Circulación y distribución geográfica', 'Perfil del lector'] # website is poorly labeled so names are needed
+TAB_NAMES = ['Información sobre el medio impreso', 'Información adicional sobre el medio impreso', 'Circulación y distribución geográfica'] # website is poorly labeled so names are needed
 
 
 def data_collector():
@@ -23,15 +26,17 @@ def data_collector():
     """
     data = {}
     for i in TAB_NAMES:
-        driver.find_element_by_xpath(f"//a[text()='{i}']").click()
-        datos = driver.find_elements_by_tag_name("p")
+        driver.find_element(by=By.XPATH, value=f"//a[text()='{i}']").click()
+        datos = driver.find_elements(By.TAG_NAME, "p")
 
         for val in range(len(datos)):
             if datos[val].text != '':
                 if len(datos[val].text.split(': ')) == 2:
                     k_v = dict([datos[val].text.split(': ')])
                     data.update(k_v)
-    return data
+
+    df = pd.DataFrame([data], columns=data.keys())
+    return df
 
 
 
@@ -45,18 +50,33 @@ def get_data_sys(driver):
     driver.find_element(by="name", value="publicacion").submit()
 
     # get list of all divs with papers
-    elements = driver.find_elements_by_class_name("resultado")
+    elements = driver.find_elements(by=By.CLASS_NAME, value='resultado')
 
     for i in range(len(elements)):  # stale element error.
-        periodicos = driver.find_elements_by_class_name("resultado")
+        periodicos = driver.find_elements(by=By.CLASS_NAME, value='resultado')
         periodicos[i].click()
 
         # get info
-        data = data_collector()
-
-        print(data)
+        retry_counter = 0
+        while True:
+            try:
+                if retry_counter <= 3:
+                    master_data = master_data.append(data_collector())
+                    print(master_data)
+                    break
+            except:
+                time.sleep(2)
+                retry_counter += 1
+                if retry_counter > 3:
+                    master_data = data_collector()
+                    print(master_data)
+                    break
+                print(f"retrying. Attempt: {retry_counter}")
+                continue
+        
         # go back
-        driver.find_element_by_class_name("regresar").click()
+        driver.find_element(by=By.CLASS_NAME, value="regresar").click()
+    return master_data
 
 
 # Chrome driver setup
@@ -69,4 +89,6 @@ chromeOptions.headless = False
 s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s, options=chromeOptions)
 
-get_data_sys(driver)
+master_data = get_data_sys(driver)
+master_data.to_csv('newspaper_data.csv', index=False)
+
